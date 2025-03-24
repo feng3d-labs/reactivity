b59fd4961c8409ba334f8d13890b23a4391cead3
feng <908087098@qq.com>
2025-03-24 22:58:35 +0800

    1

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import { watcher, WatchSession } from "@feng3d/watcher";
 import { computed, effect, reactive, ref } from "./reactivity";
-// import { computed, effect, reactive } from "@vue/reactivity";
+// import { computed, effect, reactive, ref } from "@vue/reactivity";
 
 const a = ref(1);
 const b = ref(2);
@@ -28,6 +27,14 @@ a.value = 3;
 a.value = 4;
 a.value = 1;
 console.log(result.value);
+
+
+// computed
+// index.ts:9 computed a
+// index.ts:14 computed b
+// index.ts:24 3
+// index.ts:9 computed a
+// index.ts:30 3
 
 // const watchSession: WatchSession = watcher.on()
 
