3cb9e31736778f38af0336f1d31a71997ff7b05f
feng <908087098@qq.com>
2025-03-24 09:54:50 +0800

    1

--- index.ts
+++ index.ts
@@ -1,23 +1,33 @@
-import { computed, effect, reactive } from "@vue/reactivity";
+import { watcher } from "@feng3d/watcher";
+import { reactive } from "@vue/reactivity";
 
-class A {
-    a = 1;
+const a = { a: { a: 1 } };
+const r_a = reactive(a)
+// const r_a = new Proxy(a,{})
+
+const o = { a: { a: 1 } };
+
+watcher.watchchain(o, "a", () => { });
+
+console.time("reactive");
+for (let i = 0; i < 100000; i++) {
+    r_a.a.a;
 }
-
-function getValue(a: A) {
-    const result = computed(() => {
-        // 监听
-        reactive(a).a;
-
-        // 计算
-        return a.a;
-    });
-
-    return result.value;
+console.timeEnd("reactive")
+r_a.a;
+console.time("watch");
+for (let i = 0; i < 100000; i++) {
+    o.a.a;
 }
-
-const a = new A();
-
-effect(() => {
-    console.log(getValue(a));
-});
+console.timeEnd("watch");
+console.time("reactive");
+for (let i = 0; i < 100000; i++) {
+    r_a.a.a;
+}
+console.timeEnd("reactive")
+r_a.a;
+console.time("watch");
+for (let i = 0; i < 100000; i++) {
+    o.a.a;
+}
+console.timeEnd("watch");
