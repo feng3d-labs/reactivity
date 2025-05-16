43f8f835c5d6f57f85ba7f3c2fb7c6d8c6dc2abc
feng <908087098@qq.com>
2025-05-17 02:11:25 +0800

    更新 src/index.ts 文件，调整导出顺序，简化类型导出，确保代码结构更清晰。

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { batchRun } from './batch';
 export { computed, type Computed, type ComputedReactivity } from './computed';
 export { effect, EffectReactivity, type Effect } from './effect';
 export { effectScope, EffectScope, getCurrentScope, onScopeDispose } from './effectScope';
-export { isProxy, isReactive, reactive, UnReadonly, type Reactive } from './reactive';
+export { isProxy, isReactive, reactive, type Reactive, type UnReadonly } from './reactive';
 export { forceTrack, noTrack, Reactivity } from './Reactivity';
 export { isRef, ref, RefReactivity, type Ref } from './ref';
 export { toRaw } from './shared/general';
