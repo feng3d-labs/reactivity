644f7d86259cb3470e7ef04e72a22c660c103304
feng <908087098@qq.com>
2025-10-31 02:01:32 +0800

    增强 ReactiveObject 测试用例，修复依赖时序问题，确保树形依赖处理正确，更新相关逻辑以支持多重依赖。调整 TestClass 构造函数和方法以包含新的属性 c，提升代码的可读性和功能性。

--- test/ReactiveObject.spec.ts
+++ test/ReactiveObject.spec.ts
@@ -6,7 +6,55 @@ describe('ReactiveObject', () =>
 {
     it('ReactiveObject 处理树形依赖问题，并且存在依赖时序问题', () =>
     {
-        const object = { a: 1, b: 2, d: 0 };
+        const object = { a: 1, b: 2, c: 0, d: 0 };
+        const r_object = reactive(object);
+
+        let times = 0;
+        let times1 = 0;
+
+        effect(() =>
+        {
+            times++;
+
+            r_object.a;
+
+            // 此处每次都会创建一个新的副作用，因此会执行多次执行，应避免在副作用中创建副作用
+            effect(() =>
+            {
+                r_object.c = r_object.a + r_object.b;
+
+                times1++;
+            });
+
+            r_object.d = r_object.c + object.a;
+
+        });
+
+        assert.strictEqual(times, 1);
+        assert.strictEqual(times1, 1);
+        assert.strictEqual(r_object.d, 4);
+
+        times = 0;
+        times1 = 0;
+
+        r_object.a++;
+        assert.strictEqual(r_object.d, 6);
+        assert.strictEqual(times, 1);
+        assert.strictEqual(times1, 2); // 上一次的副作用执行完成后，会创建一个新的副作用，因此会执行两次
+
+        times = 0;
+        times1 = 0;
+
+        r_object.a++;
+
+        assert.strictEqual(times, 1);
+        assert.strictEqual(times1, 3); // 前两次副作用执行完成后，会创建一个新的副作用，因此会执行三次
+        assert.strictEqual(r_object.d, 8);
+    });
+
+    it('ReactiveObject 处理树形依赖问题，并且存在依赖时序问题', () =>
+    {
+        const object = { a: 1, b: 2, c: 0, d: 0 };
         const r_object = reactive(object);
 
         let times = 0;
@@ -15,12 +63,9 @@ describe('ReactiveObject', () =>
         {
             r_object.a;
 
-            const testClass = TestClass.getInstance(object); // 放在 r_object.a 之后会出现时序问题
+            TestClass.getInstance(object); // 放在 r_object.a 之后会出现时序问题
 
-            reactive(testClass).c;
-            testClass.c;
-
-            r_object.d = testClass.c + object.a;
+            r_object.d = r_object.c + object.a;
 
             times++;
         });
@@ -82,9 +127,7 @@ const map: { [key: string]: number } = {};
 
 class TestClass extends ReactiveObject
 {
-    readonly c: number = 0;
-
-    constructor(object: { a: number, b: number })
+    constructor(object: { a: number, b: number, c: number })
     {
         super();
 
@@ -92,14 +135,13 @@ class TestClass extends ReactiveObject
         this._onMap(object);
     }
 
-    private _onCreate(object: { a: number, b: number })
+    private _onCreate(object: { a: number, b: number, c: number })
     {
-        const r_this = reactive(this);
         const r_object = reactive(object);
 
         this.effect(() =>
         {
-            r_this.c = r_object.a + r_object.b;
+            r_object.c = r_object.a + r_object.b;
         });
     }
 
@@ -112,7 +154,7 @@ class TestClass extends ReactiveObject
         });
     }
 
-    static getInstance(object: { a: number, b: number })
+    static getInstance(object: { a: number, b: number, c: number })
     {
         return this.cacheMap.get(object) || new TestClass(object);
     }
